f3aca14a4bb9c7020ae6c87e5ffd85e0c2cfe7de
陳民祥 <119960255+b1129055@users.noreply.github.com>
2022-12-27 19:08:07 +0800

    Update main1.c

--- hw6/main1.c
+++ hw6/main1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 10;
+    if (i%3 ==0){
+        printf("Love");
+        }else if (i%5 ==0){
+            printf("IU");
+        }else if (i % 15 ==0){
+            printf("Love IU");
+            }else{
+                printf (i);
+            }
+        return 0;
+}
