463eb65b50a56fdf33ffa1d7ca40f273d6867445
陳民祥 <119960255+b1129055@users.noreply.github.com>
2022-12-27 19:38:53 +0800

    Update main0.c

--- hw6/main0.c
+++ hw6/main0.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int i = 8;
+    if (i > 0 && (i & i - 1) == 0){
+        printf("%d is True",i);
+        }else{
+            printf("%d is False",i);
+        }
+    
+        return 0;
+}
